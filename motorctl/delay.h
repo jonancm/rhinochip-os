69f346c997ca52482fa032fe02ca99359588d37b
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-09-26 17:36:43 +0100

    Create main program for the MCMCU which makes LED D4 blink.

---
+++ motorctl/delay.h
@@ -0,0 +1,7 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+extern void Delay5us(int Count);
+extern void Delay5ms(int Count);
+
+#endif // DELAY_H
---
+++ motorctl/main.c
@@ -0,0 +1,35 @@
+#include <p30fxxxx.h>
+
+//_FWDT(WDT_OFF); // Turn off watchdog timer
+
+//Macros for Configuration Fuse Registers:
+//Invoke macros to set up  device configuration fuse registers.
+//The fuses will select the oscillator source, power-up timers, watch-dog
+//timers, BOR characteristics etc. The macros are defined within the device
+//header files. The configuration fuse registers reside in Flash memory.
+_FOSC(CSW_FSCM_OFF & XT_PLL8);  //Run this project using an external crystal
+                                //routed via the PLL in 8x multiplier mode
+                                //For the 7.3728 MHz crystal we will derive a
+                                //throughput of 7.3728e+6*8/4 = 14.74 MIPS(Fcy)
+                                //,~67nanoseconds instruction cycle time(Tcy).
+_FWDT(WDT_OFF);                 //Turn off the Watch-Dog Timer.
+_FBORPOR(MCLR_EN & PWRT_OFF);   //Enable MCLR reset pin and turn off the
+                                //power-up timers.
+_FGS(CODE_PROT_OFF);            //Disable Code Protection
+
+#include "delay.h"
+
+int main(void)
+{
+	// Set up port pin RB1 to drive the LED D4
+	LATBbits.LATB1 = 0;     // Clear Latch bit for RB1 port pin
+	TRISBbits.TRISB1 = 0;   // Set the RB1 pin direction to be an output
+	
+	while (1)
+	{
+		LATBbits.LATB1 = ~LATBbits.LATB1;
+		Delay5ms(100);
+	}
+	
+	return 0;
+}
