8bfc84855f6de3ccdad715fa37d233cf4704eb34
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-09-30 18:35:16 +0100

    Define function 'interpret_cmd'.

--- gpcore/shell.c
+++ gpcore/shell.c
@@ -346,3 +346,420 @@ void next_token(void)
 		}
 	}
 }
+
+void interpret_cmd(void)
+{
+	switch (cmd_name[0])
+	{
+		case 'A':
+			switch (cmd_name[1])
+			{
+				// AR: Read System Acceleration
+				case 'R':
+					break;
+				// AC: Clear Actual Position
+				case 'C':
+					break;
+				// AS: Set System Acceleration
+				case 'S':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'C':
+			switch (cmd_name[1])
+			{
+				// CC: Set Coordinate Position
+				case 'C':
+					break;
+				// CG: Enable/Disable Gripper Mode
+				case 'G':
+					break;
+				// CM: Set Motor Mode
+				case 'M':
+					break;
+				// CR: Set Robot Type
+				case 'R':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'D':
+			switch (cmd_name[1])
+			{
+				// DR: Read Motor PWM Level and Direction
+				case 'R':
+					break;
+				// DS: Set PWM Level and Direction
+				case 'S':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'F':
+			switch (cmd_name[1])
+			{
+				// FR: Receive Teach Pendant File from Host
+				case 'R':
+					break;
+				// FT: Transmit Teach Pendant File to Host
+				case 'T':
+					break;
+				// FX: Execute Teach Pendant Program
+				case 'X':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'G':
+			switch (cmd_name[1])
+			{
+				// GS: Read Gripper Status
+				case 'S':
+					break;
+				// GC: Close Gripper
+				case 'C':
+					break;
+				// GO: Open Gripper
+				case 'O':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'H':
+			switch (cmd_name[1])
+			{
+				// HR: Read Soft Home Position
+				case 'R':
+					break;
+				// HA: Got To the Hard Home Position
+				case 'A':
+					break;
+				// HG: Go To the Soft Home Position
+				case 'G':
+					break;
+				// HH: Execute a Hard Home
+				case 'H':
+					break;
+				// HL: Hard Home on Limit Switch
+				case 'L':
+					break;
+				// HS: Set Soft Home
+				case 'S':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'I':
+			switch (cmd_name[1])
+			{
+				// IB: Read Input or Swicht Bit
+				case 'B':
+					break;
+				// IP: Read Input Port
+				case 'P':
+					break;
+				// IX: Read Switch Port
+				case 'X':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'K':
+			switch (cmd_name[1])
+			{
+				// KA: Set Proportional Gain
+				case 'A':
+					break;
+				// KB: Set Differential Gain
+				case 'B':
+					break;
+				// KC: Set Integral Gain
+				case 'C':
+					break;
+				// KR: Restore User Gains from EEPROM
+				case 'R':
+					break;
+				// KS: Store User Gains to EEPROM
+				case 'S':
+					break;
+				// KX: Restore Factory Gains
+				case 'X':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'M':
+			switch (cmd_name[1])
+			{
+				// MA: Stop All Motors and Aux Ports
+				case 'A':
+					break;
+				// MC: Start Coordinated Move
+				case 'C':
+					break;
+				// MI: Start All Motors, Immediate Mode
+				case 'I':
+					break;
+				// MM: Stop Single Motor
+				case 'M':
+					break;
+				// MS: Start Single Motor
+				case 'S':
+					break;
+				// MX: Start an XYZ Move
+				case 'X':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'O':
+			switch (cmd_name[1])
+			{
+				// OB: Set Output Bit
+				case 'B':
+					break;
+				// OP: Set Output Port
+				case 'P':
+					break;
+				// OR: Read Output Port
+				case 'R':
+					break;
+				// OT: Toggle Output Bit
+				case 'T':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'P':
+			switch (cmd_name[1])
+			{
+				// PA: Read Actual Position
+				case 'A':
+					break;
+				// PW: Read Destination Position
+				case 'W':
+					break;
+				// PZ: Read XYZ Destination Position
+				case 'Z':
+					break;
+				// PD: Set Destination Position, Absolute
+				case 'D':
+					break;
+				// PR: Set Destination Position, Relative
+				case 'R':
+					break;
+				// PX: Set XYZ Destination, Absolute
+				case 'X':
+					break;
+				// PY Set XYZ Destination, Relative
+				case 'Y':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'R':
+			switch (cmd_name[1])
+			{
+				// RL: Read Limit Switches
+				case 'L':
+					break;
+				// RA: Read Proportional Gain
+				case 'A':
+					break;
+				// RB: Read Differential Gain
+				case 'B':
+					break;
+				// RC: Read Integral Gain
+				case 'C':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'S':
+			switch (cmd_name[1])
+			{
+				// SA: Read Motor Status
+				case 'A':
+					break;
+				// SC: Read System Configuration
+				case 'C':
+					break;
+				// SD: Stop/Start Delay Timer
+				case 'D':
+					break;
+				// SE: Read Host Error Stack
+				case 'E':
+					break;
+				// SM: Read Motor Mode
+				case 'M':
+					break;
+				// SP: Read Teach Pendant Error Byte
+				case 'P':
+					break;
+				// SR: Reset Motor Current Limit Circuitry
+				case 'R':
+					break;
+				// SS: Read System Status
+				case 'S':
+					break;
+				// ST: Execute Diagnostics
+				case 'T':
+					break;
+				// SU: Read Usage Time
+				case 'U':
+					break;
+				// SV: Read Version and I.D. Number
+				case 'V':
+					break;
+				// SX: Execute Diagnostics and Return Results
+				case 'X':
+					break;
+				// SZ: Read the Delay Timer Value
+				case 'Z':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'T':
+			switch (cmd_name[1])
+			{
+				// TA: Abort/Terminate Teach Pendant Program
+				case 'A':
+					break;
+				// TC: Clear Teach Pendant Display
+				case 'C':
+					break;
+				// TD: Print to Teach Pendant Display
+				case 'D':
+					break;
+				// TE: Enable/Disable Teach Pendant to Move Motors
+				case 'E':
+					break;
+				// TH: Give Control to Host
+				case 'H':
+					break;
+				// TX: Give Control to Teach Pendant
+				case 'X':
+					break;
+				// TK: Return to Host the Next Key Code
+				case 'K':
+					break;
+				// TL: Return to Host the Last Key Code
+				case 'L':
+					break;
+				// TR: Reset the Teach Pendant
+				case 'R':
+					break;
+				// TS: Set Teach Pendant Display Cursor
+				case 'S':
+					break;
+				// TT: Execute Teach Pendant Diagnostics and Return Results
+				case 'T':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'U':
+			switch (cmd_name[1])
+			{
+				// UA: Read XYZ Rotation Angle
+				case 'A':
+					break;
+				// UH: Read XYZ Home Position
+				case 'H':
+					break;
+				// UO: Read XYZ Offset
+				case 'O':
+					break;
+				// UT: Read Tool Length
+				case 'T':
+					break;
+				// UY: Read Height of Elbow Rotation Axis
+				case 'Y':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'V':
+			switch (cmd_name[1])
+			{
+				// VA: Read Motor Actual Velocity
+				case 'A':
+					break;
+				// VR: Read Motor Desired Velocity
+				case 'R':
+					break;
+				// VX: Read System Velocity
+				case 'X':
+					break;
+				// VG: Set System Velocity
+				case 'G':
+					break;
+				// VS: Set Motor Velocity
+				case 'S':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'W':
+			switch (cmd_name[1])
+			{
+				// WA: Abort all Waits
+				case 'A':
+					break;
+				// WI: Wait on Input or Switch
+				case 'I':
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'X':
+			switch (cmd_name[1])
+			{
+				// XR: Read Auxiliary Port Level and Direction
+				case 'R':
+					break;
+				// XA: Set XYZ Rotation Angle
+				case 'A':
+					break;
+				// XH: Set XYZ Home Position
+				case 'H':
+					break;
+				// XO: Set XYZ Offset
+				case 'O':
+					break;
+				// XS: Set Aux Port Level and Direction
+				case 'S':
+					break;
+				// XT: Set Tool Length
+				case 'T':
+					break;
+				// XY: Set Height of Elbow Rotation Axis
+				case 'Y':
+					break;
+				default:
+					break;
+			}
+			break;
+		default:
+			break;
+	}
+}
