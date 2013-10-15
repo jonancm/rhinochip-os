153f3d61de3ce756f2c4cc1f3b78cff5cd5dfd31
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-10-15 17:11:02 +0100

    Configure PLL for clock generator at 16x instead of 8x.

--- motorctl/main.c
+++ motorctl/main.c
@@ -7,10 +7,10 @@
 //The fuses will select the oscillator source, power-up timers, watch-dog
 //timers, BOR characteristics etc. The macros are defined within the device
 //header files. The configuration fuse registers reside in Flash memory.
-_FOSC(CSW_FSCM_OFF & XT_PLL8);  //Run this project using an external crystal
-                                //routed via the PLL in 8x multiplier mode
+_FOSC(CSW_FSCM_OFF & XT_PLL16); //Run this project using an external crystal
+                                //routed via the PLL in 16x multiplier mode
                                 //For the 7.3728 MHz crystal we will derive a
-                                //throughput of 7.3728e+6*8/4 = 14.74 MIPS(Fcy)
+                                //throughput of 7.3728e+6*16/4 = 29.50 MIPS(Fcy)
                                 //,~67nanoseconds instruction cycle time(Tcy).
 _FWDT(WDT_OFF);                 //Turn off the Watch-Dog Timer.
 _FBORPOR(MCLR_EN & PWRT_OFF);   //Enable MCLR reset pin and turn off the
