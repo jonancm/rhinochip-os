fcf1160dadcc3eb18e06f4f9468a9e5b2470aaa8
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-10-15 17:48:37 +0100

    Use ports B and C to specify motor direction.

--- motorctl/pwm.h
+++ motorctl/pwm.h
@@ -15,6 +15,13 @@
 #define PWM5    LATBbits.LATB4
 #define PWM6    LATBbits.LATB5
 
+#define DIR1    LATBbits.LATB6
+#define DIR2    LATBbits.LATB7
+#define DIR3    LATBbits.LATB8
+#define DIR4    LATCbits.LATC13
+#define DIR5    LATCbits.LATC14
+#define DIR6    LATCbits.LATC15
+
 void pwm_setup(void);
 void pwm_set_pdc1(int duty);
 
