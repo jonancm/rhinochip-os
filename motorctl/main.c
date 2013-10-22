035f8e15521bb1db3b6716f1ee8236c8d69cc1de
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-10-22 11:05:02 +0100

    Remove code from main program which sets RB0 and RB1 to be used as output pins.

--- motorctl/main.c
+++ motorctl/main.c
@@ -10,9 +10,6 @@ _FBORPOR(MCLR_DIS);              // Disable reset pin
 
 int main(void)
 {
-	// Set up port pin RB1 to drive the LED D4
-	LATBbits.LATB1 = 0;     // Clear Latch bit for RB1 port pin
-	TRISBbits.TRISB1 = 0;   // Set the RB1 pin direction to be an output
 	
 	pwm_setup();
 	pwm_set_pdc4(75);
