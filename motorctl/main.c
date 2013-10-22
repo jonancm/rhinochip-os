eb0321da72dc859e931075f534cbca82d9cf1d14
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-10-22 14:45:05 +0100

    Use the internal Fast RC Oscillator to clock the device at 30 MHz.

--- motorctl/main.c
+++ motorctl/main.c
@@ -1,21 +1,9 @@
 #include <p30fxxxx.h>
 
-//_FWDT(WDT_OFF); // Turn off watchdog timer
-
-//Macros for Configuration Fuse Registers:
-//Invoke macros to set up  device configuration fuse registers.
-//The fuses will select the oscillator source, power-up timers, watch-dog
-//timers, BOR characteristics etc. The macros are defined within the device
-//header files. The configuration fuse registers reside in Flash memory.
-_FOSC(CSW_FSCM_OFF & XT_PLL16); //Run this project using an external crystal
-                                //routed via the PLL in 16x multiplier mode
-                                //For the 7.3728 MHz crystal we will derive a
-                                //throughput of 7.3728e+6*16/4 = 29.50 MIPS(Fcy)
-                                //,~67nanoseconds instruction cycle time(Tcy).
-_FWDT(WDT_OFF);                 //Turn off the Watch-Dog Timer.
-_FBORPOR(MCLR_EN & PWRT_OFF);   //Enable MCLR reset pin and turn off the
-                                //power-up timers.
-_FGS(CODE_PROT_OFF);            //Disable Code Protection
+// Configuration settings
+_FOSC(CSW_FSCM_OFF & FRC_PLL16); // Fosc = 16x 8 MHz, Fcy = 32 MHz
+_FWDT(WDT_OFF);                  // Watchdog timer off
+_FBORPOR(MCLR_DIS);              // Disable reset pin
 
 #include "../delay.h"
 #include "pwm.h"
