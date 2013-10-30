1db1a5517fc6c180a6a050f88d6d2c09650e8947
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-10-30 18:27:34 +0000

    Implement QEI for motor E.

--- motorctl/qei.c
+++ motorctl/qei.c
@@ -353,6 +353,83 @@ void __attribute__((__interrupt__)) _T2Interrupt(void)
 	}
 	
 	// Update step count register for motor E
+	prev_encoder_state[MOTOR_E] = curr_encoder_state[MOTOR_E];
+	curr_encoder_state[MOTOR_E] = (QEA_ME << 1) | QEB_ME;
+	switch (prev_encoder_state[MOTOR_E])
+	{
+		// Previous QEA = 0, QEB = 0
+		case 0b00:
+			switch (curr_encoder_state[MOTOR_E])
+			{
+				// Current QEA = 0, QEB = 1
+				case 0b01:
+					// increasing/clockwise
+					if (++motor_steps[MOTOR_E] >= MOTOR_E_MAX_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+				// Current QEA = 1, QEB = 0
+				case 0b10:
+					// decreasing/anti-clockwise
+					if (--motor_steps[MOTOR_E] <= MOTOR_E_MIN_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+			}
+			break;
+		// Previous QEA = 0, QEB = 1
+		case 0b01:
+			switch (curr_encoder_state[MOTOR_E])
+			{
+				// Current QEA = 0, QEB = 0
+				case 0b00:
+					// decreasing/anti-clockwise
+					if (--motor_steps[MOTOR_E] <= MOTOR_E_MIN_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+				// Current QEA = 1, QEB = 1
+				case 0b11:
+					// increasing/clockwise
+					if (++motor_steps[MOTOR_E] >= MOTOR_E_MAX_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+			}
+			break;
+		// Previous QEA = 1, QEB = 0
+		case 0b10:
+			switch (curr_encoder_state[MOTOR_E])
+			{
+				// Current QEA = 0, QEB = 0
+				case 0b00:
+					// increasing/clockwise
+					if (++motor_steps[MOTOR_E] >= MOTOR_E_MAX_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+				// Current QEA = 1, QEB = 1
+				case 0b11:
+					// decreasing/anti-clockwise
+					if (--motor_steps[MOTOR_E] <= MOTOR_E_MIN_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+			}
+			break;
+		// Previous QEA = 1, QEB = 1
+		case 0b11:
+			switch (curr_encoder_state[MOTOR_E])
+			{
+				// Current QEA = 0, QEB = 1
+				case 0b01:
+					// decreasing/anti-clockwise
+					if (--motor_steps[MOTOR_E] <= MOTOR_E_MIN_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+				// Current QEA = 1, QEB = 0
+				case 0b10:
+					// increasing/clockwise
+					if (++motor_steps[MOTOR_E] >= MOTOR_E_MAX_RANGE)
+						motor_stalled[MOTOR_E] = true;
+					break;
+			}
+			break;
+	}
 	
 	// Update step count register for motor F
 	
