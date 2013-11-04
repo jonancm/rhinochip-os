edbfe8a7e58b44a2ee16ae2fcd0f244acf02c1d1
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-11-04 16:26:46 +0000

    Remove delay subroutines.

--- motorctl/main.c
+++ motorctl/main.c
@@ -5,7 +5,6 @@ _FOSC(CSW_FSCM_OFF & XT_PLL16); // Fosc = 16x 7.37 MHz, Fcy = 29.50 MHz
 _FWDT(WDT_OFF);                 // Turn off the watchdog timer
 _FBORPOR(MCLR_EN & PWRT_OFF);   // Enable reset pin and turn off the power-up timers.
 
-#include "../delay.h"
 #include "pwm.h"
 #include "qei.h"
 
