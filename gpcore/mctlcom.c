48d537a8c957f8ef30f9577f727769edc9968c87
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-11-19 17:02:10 +0000

    Adapt 'mctlcom' (from 'gpcore') to get the responses to ASCII commands instead of binary commands (using a 'hostcom'-like shell parser).

--- gpcore/mctlcom.c
+++ gpcore/mctlcom.c
@@ -60,9 +60,9 @@ void __attribute__((interrupt, auto_psv)) _T2Interrupt(void)
 
 int mctlcom_get_response(unsigned int *timeout)
 {
-	mcuicom_cmd    cmd;
-	int            i, j;
-	const int      recvd = 5;
+	char      cmd[128];
+	bool_t    full;
+	int       copied, retcode = 0;
 	
 	// If timeout was requested, start timer
 	/*
@@ -71,43 +71,32 @@ int mctlcom_get_response(unsigned int *timeout)
 	*/
 	
 	// Wait until the receive buffer has at least one full command or a timeout occurs
-	while (mcuicom_rcv_buf.used < recvd && !mctlcom_timeout);
+	while (!mcuicom_cmd_available() && !mctlcom_timeout);
+	copied = mcuicom_read_cmd(cmd_buf, CMD_BUF_SIZE, &full);
 	
-	// If timeout was requested, stop timer
-	/*
-	if (*timeout != 0)
-		mctlcom_stop_timer();
-	*/
-	
-	// If a timeout occurred, reset the timeout flag and don't do anything else
+	// If a timeout occurred, reset the timeout flag and stop the timer
 	if (mctlcom_timeout)
+	{
 		mctlcom_timeout = false;
+		//mctlcom_stop_timer();
+	}
 	// Otherwise, if a command has been fully received, get it and extract the response code
 	// in order to return it to the callee
 	else
 	{
-		cmd.data[0] = mcuicom_rcv_buf.data[0]; // opcode
-		cmd.data[1] = mcuicom_rcv_buf.data[1]; // param 0 (byte 3)
-		cmd.data[2] = mcuicom_rcv_buf.data[2]; // param 0 (byte 2)
-		cmd.data[3] = mcuicom_rcv_buf.data[3]; // param 0 (byte 1)
-		cmd.data[4] = mcuicom_rcv_buf.data[4]; // param 0 (byte 0)
-		/*
-		TODO:
-		cmd = *mcuicom_rcv_buf.data;
-		*/
+		int i;
+		bool_t error = false;
 		
-		// Disable UART 1 receive interrupts to prevent CPU from messing
-		// with the 'mcuicom_rcv_buf' buffer
-		IEC0bits.U1RXIE = 0;
-		// Shift data in the buffer to remove consumed command
-		for (i = 0, j = recvd; j < mcuicom_rcv_buf.used; ++i, ++j)
-			mcuicom_rcv_buf.data[i] = mcuicom_rcv_buf.data[j];
-		mcuicom_rcv_buf.used -= recvd;
-		// Re-enable UART 1 receive interrupts
-		IEC0bits.U1RXIE = 1;
+		for (i = 0; i < copied && !error; ++i)
+		{
+			if ('0' <= cmd[i] && cmd[i] <= '9')
+				retcode = retcode * 10 + cmd[i];
+			else
+				error = true;
+		}
 		
 		*timeout = false;
 	}
 	
-	return cmd.param[0];
+	return retcode;
 }
