0312a3a874903b0a3689b8c91d5f42681a539314
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-12-03 17:23:54 +0000

    Refactor Change Notification ISR to replace switch-block with multiple if-blocks.
    This allows the ISR to handle all limit switches in one execution.
    Previously, with the switch-block, the ISR would only handle one of the
    switches. Furthermore, it would not do anything if two switches changed at
    the same time.

--- motorctl/hardhome.c
+++ motorctl/hardhome.c
@@ -130,6 +130,7 @@ void __attribute__((interrupt, auto_psv)) _CNInterrupt(void)
 	
 	// Update current port state to the actual state
 	curr_port_state = PORTB & LMT_MSK;
+	
 	// Compute if there have been any changes in the state of the limit switches.
 	// The way to check for changes is to operate the previous state and the current
 	// state with an XOR operation. The result will have a logical 1 in every position
@@ -137,76 +138,78 @@ void __attribute__((interrupt, auto_psv)) _CNInterrupt(void)
 	// will remain set to logical 0.
 	unsigned char pin_changes = curr_port_state ^ prev_port_state;
 	prev_port_state = curr_port_state;
+	
 	// If the current port state shows changes with respect to its previous state,
 	// process accordingly.
-	switch (pin_changes)
+	if (pin_changes & LMT_MA_MSK)
 	{
-		case 0:
-			// There are no changes in any port, thus don't do anything.
-			// This should actually not happend, because the interrupt
-			// should only be triggered when there is a pin change.
-			// However, this check is done for added safety, and because
-			// we suspect the interrupt is being triggered even when there
-			// is no pin change (probably when enabling the interrupt).
-			break;
-		case LMT_MA_MSK:
-			switch (hardhome_state[MOTOR_A])
-			{
-				case HH_SEARCHING_SWITCH:
-					// If LMT = 0 -> 1, the limit switch has been found. Thus, proceed with the next step:
-					// searching point B at a low speed
-					if (LMT_MA)
-					{
-						hardhome_state[MOTOR_A] = HH_SEARCHING_B;
-						pwm_set_duty1(HARDHOME_PWM_LEVEL2);
-					}
-					break;
-				case HH_SEARCHING_B:
-					// If LMT = 1 -> 0, the limit switch has become inactive, thus, point B has been found.
-					// Proceed now with the next step: searching point A (same speed, opposite direction)
-					if (!LMT_MA)
-					{
-						point_b[MOTOR_A] = motor_steps[MOTOR_A];
-						DIR1 = ~DIR1;
-						hardhome_state[MOTOR_A] = HH_SEARCHING_A1;
-					}
-					break;
-				case HH_SEARCHING_A1:
-					// While searching for point A, the switch goes from inactive to active (LMT = 0 -> 1)
-					// and then from active to inactive (LMT = 0 -> 1). That's when point A is reached (when
-					// the second transition occurrs). The first transition must be ignored. Thus, break.
-					break;
-				case HH_SEARCHING_A2:
-					// If LMT = 1 -> 0, the limit switch has become inactive, thus, point A has been found.
-					// We're done. Now just finish the process and return control to the hard home routine.
-					if (!LMT_MA)
-					{
-						point_a[MOTOR_A] = motor_steps[MOTOR_A];
-						hardhome_state[MOTOR_A] = HH_FINISHED;
-					}
-					break;
-			}
-			break;
-		case LMT_MB_MSK:
-			// Not yet implemented
-			// TODO: implement
-			break;
-		case LMT_MC_MSK:
-			// Not yet implemented
-			// TODO: implement
-			break;
-		case LMT_MD_MSK:
-			// Not yet implemented
-			// TODO: implement
-			break;
-		case LMT_ME_MSK:
-			// Not yet implemented
-			// TODO: implement
-			break;
-		case LMT_MF_MSK:
-			// Not yet implemented
-			// TODO: implement
-			break;
+		//mcuicom_send("Change in LMT_MA\n", STRLEN("Change in LMT_MA\n"));
+		switch (hardhome_state[MOTOR_A])
+		{
+			case HH_SEARCHING_SWITCH:
+				// If LMT = 0 -> 1, the limit switch has been found. Thus, proceed with the next step:
+				// searching point B at a low speed
+				if (LMT_MA)
+				{
+					hardhome_state[MOTOR_A] = HH_SEARCHING_B;
+					pwm_set_duty1(HARDHOME_PWM_LEVEL2);
+				}
+				break;
+			case HH_SEARCHING_B:
+				// If LMT = 1 -> 0, the limit switch has become inactive, thus, point B has been found.
+				// Proceed now with the next step: searching point A (same speed, opposite direction)
+				if (!LMT_MA)
+				{
+					point_b[MOTOR_A] = motor_steps[MOTOR_A];
+					DIR1 = ~DIR1;
+					hardhome_state[MOTOR_A] = HH_SEARCHING_A1;
+				}
+				break;
+			case HH_SEARCHING_A1:
+				// While searching for point A, the switch goes from inactive to active (LMT = 0 -> 1)
+				// and then from active to inactive (LMT = 0 -> 1). That's when point A is reached (when
+				// the second transition occurrs). The first transition must be ignored. Thus, break.
+				break;
+			case HH_SEARCHING_A2:
+				// If LMT = 1 -> 0, the limit switch has become inactive, thus, point A has been found.
+				// We're done. Now just finish the process and return control to the hard home routine.
+				if (!LMT_MA)
+				{
+					point_a[MOTOR_A] = motor_steps[MOTOR_A];
+					hardhome_state[MOTOR_A] = HH_FINISHED;
+				}
+				break;
+		}
+	}
+	
+	if (pin_changes & LMT_MB_MSK)
+	{
+		// Not yet implemented
+		// TODO: implement
+	}
+	
+	if (pin_changes & LMT_MC_MSK)
+	{
+		// Not yet implemented
+		// TODO: implement
+	}
+	
+	if (pin_changes & LMT_MD_MSK)
+	{
+		// Not yet implemented
+		// TODO: implement
+	}
+	
+	if (pin_changes & LMT_ME_MSK)
+	{
+		// Not yet implemented
+		// TODO: implement
+	}
+	
+	if (LMT_MF_MSK)
+	{
+		// Not yet implemented
+		// TODO: implement
 	}
 	
 	// Re-enable Change Notification interrupts
