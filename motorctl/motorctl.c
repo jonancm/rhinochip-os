f99028125124f276669a464356dc38daa6d31b3f
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-12-18 10:58:54 +0000

    Create data structure to hold PID information and implement a PID controller loop.

--- motorctl/motorctl.c
+++ motorctl/motorctl.c
@@ -1,5 +1,53 @@
 #include "motorctl.h"
 
+/********************
+ * PID control loop *
+ ********************/
+
+#define PWM_MIN_DUTY    0
+#define PWM_MAX_DUTY    100
+
+typedef struct {
+	float prev_error; // error at time k-1 (previous error value)
+	float curr_error; // error at time k   (current error value)
+	float error_sum;  // sum of all the previous error values (integral term)
+	float KP;         // proportional gain (P)
+	float KI;         // integral gain     (I)
+	float KD;         // differential gain (D)
+} pid_info_t;
+
+pid_info_t    pid_info[NUM_MOTORS];
+bool_t        pid_enabled[NUM_MOTORS];
+
+int pid_loop(pid_info_t *pid_info, int current_pos, int desired_pos)
+{
+	float    error_diff;
+	int      pid_output;
+	
+	pid_info->curr_error = desired_pos - current_pos;
+	pid_info->error_sum += pid_info->curr_error;
+	
+	error_diff = pid_info->curr_error - pid_info->prev_error;
+	
+	pid_output = pid_info->KP * pid_info->curr_error
+	           + pid_info->KI * pid_info->error_sum
+	           + pid_info->KD * error_diff;
+	
+	/*
+	if (PWM_MIN_DUTY <= pid_output && pid_output <= PWM_MAX_DUTY)
+		pid_info->error_sum = tmpi;
+	else
+	*/
+	if (pid_output < PWM_MIN_DUTY)
+		pid_output = PWM_MIN_DUTY;
+	else if (PWM_MAX_DUTY < pid_output)
+		pid_output = PWM_MAX_DUTY;
+	
+	pid_info->prev_error = pid_info->curr_error;
+	
+	return pid_output;
+}
+
 /***************************
  * Motor control functions *
  ***************************/
