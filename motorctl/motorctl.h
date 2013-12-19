e514897ef91aa80afd9ad25da99ab59ce6541ac3
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2013-12-19 18:31:33 +0000

    Reduce frequencies of Timer 2 (QEI) and Timer 3 (PID) to 10 KHz and 5 KHz, respectively.

--- motorctl/motorctl.h
+++ motorctl/motorctl.h
@@ -1,8 +1,8 @@
 #ifndef MOTORCTL_H
 #define MOTORCTL_H
 
-#define T3FREQ                10000                          /* Timer 3 frequency: 10 kHz */
-#define T3PERIOD              (1. / T3FREQ)                  /* Timer 3 period: 0.1 ms (0.0001 seconds) */
+#define T3FREQ                5000                           /* Timer 3 frequency: 5 kHz */
+#define T3PERIOD              (1. / T3FREQ)                  /* Timer 3 period: 0.2 ms (0.0002 seconds) */
 #define T3PRESCALER           1                              /* Timer 3 prescale value of 1:1 */
 #define PR3VAL                (FCY / (T3FREQ * T3PRESCALER)) /* Value for the PR1 register of Timer 3 */
 
