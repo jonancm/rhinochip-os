efbef7472504534f908d3b5ad3e2e823252deee2
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2014-01-10 19:54:25 +0000

    Remove debug code.

--- motorctl/main.c
+++ motorctl/main.c
@@ -12,13 +12,6 @@ _FBORPOR(MCLR_EN & PWRT_OFF);   // Enable reset pin and turn off the power-up ti
 #include "motorctl.h"
 
 #include "../debug.h"
-#ifndef NDEBUG
-#include <stdio.h>
-#define INTERPRET_CMDS ""
-#define CLEAR_DISPLAY "\x1B\x5B" "2J" // Erase the screen: ESC [ Ps J
-#define CURSOR_HOME "\x1B\x5BH" // ESC [ H
-#define DISPLAY_CMDS ""
-#endif
 
 int main(void)
 {
@@ -31,10 +24,6 @@ int main(void)
 	// is ready and working fine.
 	dbgmsg_uart1("UART 1 MCMCU ready\n");
 	
-	#ifndef NDEBUG
-	printf(CLEAR_DISPLAY);
-	#endif
-	
 	while (1)
 	{
 		// TODO: two approaches possible, compare and select the best.
@@ -46,11 +35,6 @@ int main(void)
 		//    has greater priority over command interpretation (which can actually
 		//    be less efficient).
 		gpcorecom_interpret_next();
-		
-		#ifndef NDEBUG
-		printf(INTERPRET_CMDS    CURSOR_HOME    "% 6d"    DISPLAY_CMDS,
-		       motor_steps[MOTOR_A]);
-		#endif
 	}
 	
 	return 0;
