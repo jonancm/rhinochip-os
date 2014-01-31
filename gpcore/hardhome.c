9409c6c0a636ffb046f1935874155a7758b311d1
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2014-01-31 09:46:43 +0000

    Remove Change Notification code from the hard home routine, since it's not used anymore.

--- gpcore/hardhome.c
+++ gpcore/hardhome.c
@@ -22,18 +22,6 @@ inline void lmtswitch_setup(void)
 	// Disable analog input on pins RB0..RB5 and enable digital input instead
 	
 	ADPCFG |= 0x003F;
-	
-	/********************************************************
-	 * Set up Change Notification for the involved I/O pins *
-	 ********************************************************/
-	
-	// Enable RB0..RB5 to interrupt the CPU on input change (CN2..CN7)
-	
-	CNEN1 |= 0x00FC;
-	
-	// Clear the Change Notification interrupt flag
-	
-	IFS0bits.CNIF = 0;
 }
 
 inline void hardhome(void)
@@ -44,22 +32,6 @@ inline void hardhome(void)
 	hardhome_motor_d();
 	hardhome_motor_e();
 	hardhome_motor_f();
-}
-
-/**
- * Flag to tell the hard home routine that the switch has been reached.
- */
-bool_t switch_found = false;
-
-/**
- * Change Notification ISR
- */
-void __attribute__((interrupt, auto_psv)) _CNInterrupt(void)
-{
-	// Enable flag to tell the hard home routine that the switch has been reached
-	switch_found = true;
-	// Clear the Change Notification interrupt flag
-	IFS0bits.CNIF = 0;
 }
 
 int get_motor_pos(char motor)
@@ -97,9 +69,6 @@ void hardhome_motor_a(void)
 	// Change Notification interrupt is triggered.
 	if (!LMT_MA)
 	{
-		// Enable Change Notification interrupts
-		IEC0bits.CNIE = 1;
-		
 		// Set PWM level of motor A to 100% duty cycle
 		mcuicom_send("PA,100" CMDEND);
 		
@@ -107,17 +76,10 @@ void hardhome_motor_a(void)
 		mcuicom_send("MP" CMDEND);
 		
 		// Wait until the switch is found
-		//while (!switch_found);
 		while (!LMT_MA);
 		
 		// Stop motor
 		mcuicom_send("SA" CMDEND);
-		
-		// Disable Change Notification interrupts
-		IEC0bits.CNIE = 0;
-		
-		// Reset flag to allow more hard homes to be executed
-		switch_found = false;
 	}
 	
 	// Clear position register to make PID take the current position as its reference (zero) position
