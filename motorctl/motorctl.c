cb988828da2084a3f94ff4ad829400bf4ff7481b
Jonan CM <jonan.cruz101@alu.ulpgc.es>
2014-02-19 17:14:04 +0000

    Fix to ensure that calculations are performed in floating point arithmetic.

--- motorctl/motorctl.c
+++ motorctl/motorctl.c
@@ -91,7 +91,7 @@ void setup_trapezoidal_movement(void)
 {
 	// Initialize motor control data structure for motor A
 	motorctl_info[MOTOR_A].enabled = false;
-	motorctl_info[MOTOR_A].wdes = (motor_desired_velocity[MOTOR_A] / 100) * (system_velocity / 100.0) * motor_max_velocity[MOTOR_A];
+	motorctl_info[MOTOR_A].wdes = (motor_desired_velocity[MOTOR_A] / 100.) * (system_velocity / 100.) * motor_max_velocity[MOTOR_A];
 	motorctl_info[MOTOR_A].alpha = (system_acceleration / 100.) * motorctl_info[MOTOR_A].wdes;
 	motorctl_info[MOTOR_A].theta0 = motor_steps[MOTOR_A];
 	motorctl_info[MOTOR_A].thetaf = motor_commanded_pos[MOTOR_A];
